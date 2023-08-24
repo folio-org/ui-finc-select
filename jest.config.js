d5e8399461995154c8854786f1d216934bb8991d
alb3rtino <alb3rtino@users.noreply.github.com>
2023-08-24 22:16:52 +0200

    Use @folio/jest-config-stripes Remove unused msw

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,10 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const path = require('path');
+const config = require('@folio/jest-config-stripes');
 
-// add '@k-int' for test in FilterFileForm.test.js
-// in DocumentsFieldArray.js we import { useKiwtFieldArray } from '@k-int/stripes-kint-components';
-const esModules = ['@folio', 'ky', '@k-int'].join('|');
+const esModules = ['@folio', '@k-int', 'ky', 'uuid'].join('|');
 
 module.exports = {
-  collectCoverageFrom: [
-    '**/(lib|src)/**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/test/**',
-  ],
-  coverageDirectory: './artifacts/coverage-jest/',
-  coverageReporters: ['lcov'],
-  reporters: ['jest-junit', 'default'],
-  transform: { '^.+\\.(js|jsx)$': path.join(__dirname, './test/jest/jest-transformer.js') },
+  ...config,
+  setupFiles: [...config.setupFiles, path.join(__dirname, './test/jest/setupFiles.js')],
   transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
-  moduleNameMapper: {
-    '^.+\\.(css)$': 'identity-obj-proxy',
-    '^.+\\.(svg)$': 'identity-obj-proxy',
-  },
-  testEnvironment: 'jsdom',
-  testMatch: ['**/(lib|src)/**/?(*.)test.{js,jsx}'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  setupFiles: [path.join(__dirname, './test/jest/setupTests.js')],
-  setupFilesAfterEnv: [path.join(__dirname, './test/jest/jest.setup.js')],
 };
